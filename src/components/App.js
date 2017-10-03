9649bda8a1f209a0104be1d53d96d7cc9064b058
JamesRiall <jamesrariall@gmail.com>
2017-10-03 08:17:45 +0100

    completed up to lesson 15

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,48 @@ import React from "react";
 import Header from "./Header.js";
 import Order from "./Order.js";
 import Inventory from "./Inventory.js";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    //update our state
+    const fishes = { ...this.state.fishes };
+    //add in our new fish
+    const timeStamp = Date.now();
+    fishes[`fish-${timeStamp}`] = fish;
+    //set state
+    this.setState({ fishes });
+    //same as this.setState({ fishes: fishes });
+  }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
   render() {
-    {console.log(this)};
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="list-of-fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+          </ul>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     );
   }
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,15 +2,33 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  goToStore(event) {
+    event.preventDefault();
+    console.log('works');
+    const storeId = this.storeInput.value;
+    console.log('Going to ${storeId}');
+    this.context.router.transitionTo('store/${storeId}');
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
         <button type="submit">Visit Store</button>
       </form>
     )
   }
+}
+
+//making router available from the App level import of router using context
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
